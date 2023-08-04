826e3bd204f2506ae2f8766299e52ce142094b74
Chih-yuHung <chih-yu.hung@mail.mcgill.ca>
2023-08-04 18:21:18 -0400

    add two files 1. Bubble chart for applicatiom time 2. bar chart for soil texture

--- Data analysis_field emissions.R
+++ Data analysis_field emissions.R
@@ -5,6 +5,7 @@ library(reshape2)
 library(scatterpie)
 library(stringr)
 library(ggpubr)
+library(openxlsx2)
 #read data
 GHG <- read.csv("input/data of ghg emission.csv",header = T)
 
@@ -12,12 +13,15 @@ GHG <- read.csv("input/data of ghg emission.csv",header = T)
 GHG.field <- GHG[grepl("Field", GHG$GHG.source),]
 GHG.field <- GHG.field[!grepl("^Grass$", GHG.field$Field.crop),]
 GHG.field <- GHG.field[GHG.field$N2O==TRUE,]
-
-#NIR N2O data
+#replace "National" to every provinces
+National<-paste(unique(GHG.field$Region)[c(-1,-3)], collapse = ", ")
+GHG.field$Region[GHG.field$Region == "National"] <- National
+#NIR N2O data, convert to kT CO2e
 N2O <- read.csv("input/N2O emission_NIR.csv", header = TRUE) %>%
   slice(c(2, 1, 9, 10, 7, 4, 3, 5, 6, 8, 11:16)) %>%
   mutate(across(2:11, as.numeric)) %>%
-  mutate(across(2:11, round, digits = 3)) 
+  mutate(across(2:11, ~.* 1000)) %>%
+  mutate(across(2:11, round, digits = 1)) 
 
 N2O[17,2:11] <- colSums(N2O[c(7:16),2:11])
 N2O[17,1] <- "Other"
@@ -26,7 +30,6 @@ N2O$Total <- rowSums(N2O[,2:11])
 N2O[8,2:12] <- colSums(N2O[,2:12])
 N2O[8,1] <- "Total"
 
-
 #convert to percentage
 N2O.p <- as.data.frame(round(proportions(as.matrix(N2O[,2:11]),1)*100,1))
 row.names(N2O.p) <- N2O[,1] 
@@ -34,19 +37,22 @@ N2O.p <- N2O.p %>%
   select(2,1,10,3,7,9,4,6,8,5)
 #N2O.p$Total <- round(rowSums(N2O.p[,1:10]),0)
 
+#change the province sequence, must do this after obtaining percentage
+N2O <- N2O %>%
+  select(3,2,11,4,8,10,5,7,9,6)
+
 
 #province frequency
 Prov <- unlist(strsplit(as.character(GHG.field$Region), ", "))
 prov_freq <- table(Prov)
-prov_freq[12] <- sum(prov_freq)
-prov_freq <- prov_freq[c(2,1,11,3,8,10,5,7,9,6,12)]
+prov_freq[11] <- sum(prov_freq)
+prov_freq <- prov_freq[c(2,1,10,3,7,9,4,6,8,5,11)]
 
 #Livestock types
 Livestock <- unlist(strsplit(as.character(GHG.field$Livestock), ", "))
 live_freq <- table(Livestock)
-live_freq <- c(live_freq[c(1,2,6,4,3,5)],0,sum(live_freq))
-         
-#Beef 28, Dairy 56, Horse 5, poultry 11, sheep 5. tot:138 
+live_freq <- c(live_freq[c(1,2,6,4,3,5)],0,sum(live_freq)) #included national study
+#Beef 31, Dairy 60, swine 37, Horse 5, poultry 11, sheep 6, tot:150 (one NAs)
 
 Live.prov <- GHG.field %>%
   separate_rows(Region, sep = ", ") %>%
@@ -54,23 +60,25 @@ Live.prov <- GHG.field %>%
   unnest(Livestock) %>%
   count(Region, Livestock) %>%
   pivot_wider(names_from = Region, values_from = n, values_fill = 0) %>%
-  slice(c(1,2,7,3,4,5)) %>%
-  relocate("National",.after = last_col()) %>%
+  slice(c(1,2,6,4,3,5,7)) %>%
+  {.[7,2] <- 0; .} %>% #replace the NA to 0
   mutate(Total = rowSums(across(-Livestock, .names = "n_{.col}"))) %>%
   # add a row for other, no other in my study
-  rbind(0)  %>%
   {.[7, 1] <- "Other"; .} %>%
-  add_row(Livestock = "Total", !!!colSums(.[2:13]))
+  add_row(Livestock = "Total", !!!colSums(.[2:12]))
 
 
 #convert to percentage
 Live.prov.p <- as.data.frame(round(proportions(as.matrix(Live.prov[1:8,2:11]),1)*100,1))
-Live.prov.p <- Live.prov.p %>%
-  select(c(2,1,10,3,7,9,4,6,8,5)) %>%
-  slice(c(1,2,6,4,3,5,7,8))
-Live.prov.p[7,] <- 0 
-colnames(Live.prov.p) <- colnames(N2O.p)[1:10]
 rownames(Live.prov.p) <- row.names(N2O.p)
+Live.prov.p[7,] <- 0 
+Live.prov.p <- Live.prov.p %>%
+  select(c(2,1,10,3,7,9,4,6,8,5))
+colnames(Live.prov.p) <- colnames(N2O.p)[1:10]
+
+#Do this after obtaining percentage
+Live.prov <- Live.prov %>%
+  select(c(3,2,11,4,8,10,5,7,9,6))
 
 Table1 <- rbind(Live.prov.p,N2O.p)
 
@@ -84,37 +92,24 @@ Table1 <- rbind(Live.prov.p,N2O.p)
 # }
 #add the study frequency
 
-Table1 <- rbind(Table1,prov_freq)
+Table1 <- rbind(Table1,prov_freq) #the last variable, total was not added to Table 1
 rownames(Table1)[17] <- "Study freq"
 
 #Export file
-write.csv(Table1,file = "output/Table1.csv",
-          col.names = T, row.names = T)
+write_xlsx(list(Table1=Table1,N2O = N2O,study = Live.prov),
+           file = "output/Table1_N2O only.xlsx",
+           col.names = T, row.names = T)
 
 
-#Soil texture data
-texture_count <- GHG.field %>%
-  filter(Texture != "NA") %>%
-  filter(str_detect(Technique, "Micrometeorology")|
-           str_detect(Technique, "Soil chamber")) %>%
-  separate_rows(Region, sep = ", ") %>%
-  separate_rows(Texture, sep= ", ") %>%
-  group_by(Region, Texture) %>%
-  count()
 
 
-#Application 
 
-GHG.application <- GHG.field %>%
-  filter(Application.time != "NA") %>%
-  filter(str_detect(Technique, "Micrometeorology")|
-          str_detect(Technique, "Soil chamber")) %>%
-  separate_rows(Manure.type, sep = ", ") %>%
-  separate_rows(Season, sep = ", ") %>%
-  separate_rows(Application.time, sep = ", ") %>%
-  group_by(Application.time, Season, Manure.type) %>%
-  summarise(Number = n()) %>%
-  ungroup()
+
+
+
+
+
+
 
 
 
@@ -137,7 +132,13 @@ GHG.both <- GHG.field[grep(("Liquid, Solid"),GHG.field$Manure.type),] #42
 
 
 #GHG types
-sum(GHG.field$CO2) #64
-sum(GHG.field$CH4) #41
-sum(GHG.field$N2O) #122
-sum(GHG.field$NH3) #16
+sum(GHG.field$CO2) #59, 47.2%
+sum(GHG.field$CH4) #34, 27.2%
+sum(GHG.field$N2O) #109, 87.2%
+sum(GHG.field$NH3) #13
+
+#To know how many studies quantify the contirbution of methane and CO2 for year-round emissions
+GHG.CH4yearround <- GHG.field %>%
+                    filter(str_detect(Season, "Year-Round")&
+                    str_detect(CH4, "TRUE") &
+                    str_detect(CO2, "TRUE") ) #Only 193 Ellert had measurements
